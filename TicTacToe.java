06b1d301b28e402708ee7ec83f6ceb13086bb329
isbees <isbeesbrukhman@gmail.com>
2017-04-22 14:43:48 -0400

    update

--- TicTacToe.java
+++ TicTacToe.java
@@ -18,7 +18,7 @@ public class TicTacToe {
           }
     }}
   }
-   public boolean verticalWin() {  // checks the board Vertically(row) for a win
+   public boolean vCheck() {  // checks the board Vertically(row) for a win
 	  for(int c = 0; c < board[0].length; c++)
 	  {
 		  if(board[0][c].equals("X") && board[1][c].equals("X") && board[2][c].equals("X"))
@@ -59,7 +59,41 @@ public class TicTacToe {
   
   }
   //AI ------------------------------------------------------------------------
-  public void AiMove(){ //AI will pick from 3 random points and choose where to go
+
+   public boolean AIvCheck() {  // checks the board Vertically(row) for a win
+	  for(int c = 0; c < board[0].length; c++) {
+		  if(board[0][c].equals("X") && board[1][c].equals("X") && board[2][c].equals("X")) {
+			  aiWin = true;
+			  return true;
+		  }
+		  else if(board[0][c].equals("O") && board[1][c].equals("O") && board[2][c].equals("O")) {
+			  playerWin = true; 
+			  return true;
+		  }		  
+	  }
+ 
+	  return false;
+}
+  public void AIhCheck(){ // checks the board Horizontally(column) for a win
+    
+	  for(int r = 0; r < board.length; r++)
+	  {
+		  if(board[r][0].equals("X") && board[r][1].equals("X") && board[r][2].equals("X"))
+		  {
+			  aiWin = true;
+			  return true;
+		  }
+		  else if(board[r][0].equals("O") && board[r][1].equals("O") && board[r][2].equals("O"))
+		  {
+			  playerWin = true; 
+			  return true;
+		  }
+			  
+	  }
+ 
+	  return false;
+  }  
+public void AiMove(){ //AI will pick from 3 random points and choose where to go
     int[] p1 = {0,0};
     int[] p2 = {0,0};
     int[] p3 = {0,0};
