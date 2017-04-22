b6be2418913b4f42cf8b691418c5278b2248d178
isbees <isbeesbrukhman@gmail.com>
2017-04-22 12:48:59 -0400

    I added player methods and private instance variables

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class TicTacToe {
-  public static void main(String[] args) {
+  // Variables ----------------------------------------------------------------
+  private int rows, cols;
+  private String[][] board = new String[rows][cols];
+  private boolean game = false;
+ 
+  public TicTacToe(){ // when called it starts a new game
+    game = true;
+  }  
+  //AI ------------------------------------------------------------------------
+  public void AiMove(){
+   if() 
+  }
+  //Player methods ------------------------------------------------------------
+  public void setBoard(int r, int c){ //Sets the size of the board
+   rows = r;
+   cols = c;
+  }
+   public void mark(int r, int c){ //Marks a certain spot. PLAYERS ARE Os
+   board[r][c] = "O";
+  }
   
+  public static void main(String[] args) {
+  int x;
+  Scanner in = new Scanner(System.in);
+    
+    
   }
 }
