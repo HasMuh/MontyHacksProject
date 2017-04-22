3356a67ea48a757edb0749da1962c7061a244533
Hasan Muhammad <hsnmuh47@gmail.com>
2017-04-22 18:02:59 -0400

    General fixes

--- TicTacToe.java
+++ TicTacToe.java
@@ -30,16 +30,19 @@ public class TicTacToe {
 	}
 	}
   // Variables ----------------------------------------------------------------
-  private int rows, cols;
+   private int rows, cols;
   private String[][] board;
-  private boolean aiWin = false;
-  private boolean playerWin = false;
-  private boolean aiMove = false;
+  private boolean game;
+  private boolean aiWin;
+  private boolean playerWin;
+  private boolean aiMove;
   private int[] p1;
   private int[] p2;
   private int[] p3;
+  private Scanner in;
  
   public TicTacToe(){ // when called it starts a new game
+    game = true;   
     board = new String[3][3];
     for(int r = 0; r < board.length; r++)
     {
@@ -51,17 +54,17 @@ public class TicTacToe {
     aiWin = false;
     playerWin = false;
     aiMove = false;
+    in = new Scanner(System.in);
   }  
   public int countEmpt(){ // counts the empty spaces left on the board
     int spaces = 0;
     for(int x = 0; x < board.length; x++){ //rows
         for(int y = 0; y < board[0].length; y ++){ //collums
-          if(board[x][y].equals("")){
+          if(board[x][y].equals(" ")){
            spaces ++; 
           }
     }}
 	  return spaces;
-  }
    public int[][] getBoard(){
 	return board;
    }
@@ -71,9 +74,9 @@ public class TicTacToe {
    public boolean getPlayerWin(){
 	   return playerWin;
    }
-	public boolean getaiMove(){
-	return aiMove;
-	}
+   public boolean getaiMove(){
+   return aiMove;
+   }
    public boolean vCheck() {  // checks the board Vertically(row) for a win
 	  for(int c = 0; c < board[0].length; c++)
 	  {
@@ -225,21 +228,50 @@ public class TicTacToe {
 public void AiMove(){ //AI will pick from 3 random points and choose where to go
 	Random n = new Random();
 	int choice = n.nextInt(3); // picks one of the random points to be used by ai
+	int count = 0;
+	for(int r = 0; r < board.length; r++)
+	{
+		for(int c = 0; c < board[0].length; c++)
+		{
+			if(!(board[r][c].equals("X") || board[r][c].equals("O")))
+			{
+				if(count == 0)
+				{
+					p1[0] = r;
+					p1[1] = c;
+					count++;
+				}
+				if(count == 1)
+				{
+					p2[0] = r;
+					p2[1] = c;
+					count++;
+				}
+				if(count == 2)
+				{
+					p3[0] = r;
+					p3[1] = c;
+					count++;
+				}
+			}
+		}
+	}
 	int[][] points = {
-			 {p1},
-			 {p2},
-			 {p3}
+			 {p1[0],p1[1]},
+			 {p2[0],p2[1]},
+			 {p3[0],p3[1]}
 	};
 	
-   if(board.spaces >= 3) {
-  	board[points[choice]] = "X";
-      } e/lse if(board.spaces >= 2) { 
+   if(this.countEmpt() >= 3) {
+	   board[points[choice][0]][points[choice][1]] = "X";
+      } 
+   else if(this.countEmpt() >= 2) { 
 	   choice = n.nextInt(2);
-	   board[points[choice]] = "X";
-   	 } else if(board.spaces >= 1){
-	   board[points[1]] = "X";
+	   board[points[choice][0]][points[choice][1]] = "X";
+   	 } 
+   else if(this.countEmpt() >= 1){
+	   board[points[0][0]][points[0][1]] = "X";
       }// end of the 3 random point if statement
-	aiMove = false;
   }
   //Player methods ------------------------------------------------------------
   public void setBoard(int r, int c){ //Sets the size of the board
@@ -247,7 +279,22 @@ public void AiMove(){ //AI will pick from 3 random points and choose where to go
    cols = c;
   }
    public void mark(int r, int c){ //Marks a certain spot. PLAYERS ARE Os
+	boolean legal = true;   
+   if(board[r][c].equals("X") || board[r][c].equals("O"))
+   {
+	   System.out.println("Illegal Move, try again");
+	   System.out.print("Enter row: ");
+	   int rNew = in.nextInt();
+	   System.out.print("Enter column: ");
+	   int cNew = in.nextInt();
+	   legal = false;
+	   mark(rNew,cNew);
+	   
+   }
+   if(legal)
+   {
    board[r][c] = "O";
+   }
   }
 }
 
